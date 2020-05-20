eb6a97e4174d99e7907b567c5b95629f34ac94ae
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-05-20 20:01:34 +0900

    update our menu-items to navigate withRouter HOC

--- src/components/directory/directory.components.jsx
+++ src/components/directory/directory.components.jsx
@@ -49,9 +49,11 @@ class Directory extends React.Component {
     return (
       <div className="directory-menu">
         {
-          this.state.sections.map( ({title, imageUrl, id, size}) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-          ))
+          this.state.sections.map( ({id, ...otherSectionProps}) => (
+            <MenuItem key={id} {...otherSectionProps} />
+            //{title, imageUrl, id, size, linkUrl} => othersectionprops will take care of the duplicate name props in the callingout function
+            //those value in the function will be called automatically through the data
+            ))
         }
       </div>
     )
