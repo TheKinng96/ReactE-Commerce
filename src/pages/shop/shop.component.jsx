ff8fd0cf192fa4280cdab446b5858c68748c4abc
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-05-24 17:51:59 +0900

    created a collection overview of the shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import SHOP_DATA from './shop.data'
-import CollectionPreview from '../../components/collection-preview/collection-preview'
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+import CollectionsOverview from '../../components/collection-overview/collection-overview.component'
 
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
+const ShopPage =({ collections }) => (
+  <div className='shop-page'>
+    <CollectionsOverview />
+  </div>
+);
 
-  render(){ 
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>);
-  }
-}
+
 
 export default ShopPage;
