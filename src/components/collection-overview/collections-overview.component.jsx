b8b93f6f0aef84a82e92e868aa7cd0b576e13dba
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-05-30 22:12:50 +0900

    corrected components name

--- src/components/collection-overview/collections-overview.component.jsx
+++ src/components/collection-overview/collections-overview.component.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import CollectionPreview from '../collection-preview/collection-preview';
+import CollectionPreview from '../collection-preview/collection-preview.component';
 
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selectore';
-import { CollectionsOverviewContainer } from './collection-overview.styles';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+import { CollectionsOverviewContainer } from './collections-overview.styles';
 
 const CollectionsOverview = ({ collections }) => (
   <CollectionsOverviewContainer>
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectDirectorySections } from '../../redux/directory/directory.selectors'
 
-import MenuItem from '../menu-item/menu-item.components';
+import MenuItem from '../menu-item/menu-item.component';
 import { DirectoryMenuContainer } from './directory.styles'
 //directory needs to store state value of menu items which use to pass and create menu item component
 const Directory = ({ sections }) => (
